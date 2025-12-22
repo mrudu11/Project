f4b081143812a6e2c9ad14fb800a0e9676d5c994
Aditi Mane <aditisangram1702@gmail.com>
2025-12-22 10:35:45 +0530

    My local changes before merging upstream my local changes are into (pool,video,server,package file)

--- ProjectBackend/server.js
+++ ProjectBackend/server.js
@@ -1,9 +1,13 @@
-const express = require("express")
-const studentRouter = require("./routes/courses")
+const express = require("express");
+const studentRouter = require("./routes/courses");
+const vdoRouter = require("./routes/video");
+const studRouter = require("./routes/video");
 
- const app = express()
- app.use(express.json())
- app.use(studentRouter)
- app.listen(4000,'localhost',()=>{
-    console.log("server started at port 4000")
- })
+const app = express();
+app.use(express.json());
+app.use("/students", studentRouter);
+app.use("/vdo", vdoRouter);
+app.use("/stud", studRouter);
+app.listen(4000, "localhost", () => {
+  console.log("server started at port 4000");
+});
